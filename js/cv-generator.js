cbad08a6f24e2ea2e0f709a9cdb339c9d7a75a34
Aymen-Khazrii <aymen.khazri@myyahoo.com>
2026-01-21 17:47:27 +0100

    Update cv-generator.js

--- js/cv-generator.js
+++ js/cv-generator.js
@@ -1,1 +1,13 @@
+document.getElementById("cvType").addEventListener("change", e => {
+  const type = e.target.value;
 
+  document.body.classList.remove("data-cv", "business-cv");
+
+  if (type === "data") {
+    document.body.classList.add("data-cv");
+  }
+
+  if (type === "business") {
+    document.body.classList.add("business-cv");
+  }
+});
